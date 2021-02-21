89d1a4a3dfa407bdb09e889b7f5230a173341545
James Simmonds <james.e.simmonds@gmail.com>
2021-02-21 10:18:48 +0000

    int-part definition

--- surds.clj
+++ surds.clj
@@ -9,10 +9,18 @@
    (+ (first surd) (math/sqrt (second surd)))
    (nth surd 2)))
 
+(defn int-part
+  [surd]
+  (int (evaluate surd)))
+
 (deftest surd-evaluation
   (testing "Evaluating surds"
     (is (= (evaluate [-2 7 1]) 0.6457513110645907))
     (is (= (evaluate [2 7 1]) 4.645751311064591))
-    (is (= (evaluate [2 7 3]) 1.5485837703548635))))
+    (is (= (evaluate [2 7 3]) 1.5485837703548635)))
+  (testing "Calculating integer parts"
+    (is (= (int-part [0 7 1]) 2))
+    (is (= (int-part [2 7 3]) 1))
+    (is (= (int-part [2 7 1]) 4))))
 
-;; (run-tests 'surds)
+(run-tests 'surds)
