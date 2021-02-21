1e43ac6b39b4d8edfcf33c1fc419145931b53452
James Simmonds <james.e.simmonds@gmail.com>
2021-02-21 17:02:32 +0000

    Working implementation table

--- src/application.clj
+++ src/application.clj
@@ -1,6 +1,17 @@
-(ns application)
+(ns application
+  (:require [clojure.pprint :as p]))
 
-(load-file "./surds.clj")
+(load-file "./cont-frac.clj")
 
-(println (surds/evaluate [-1 7 3]))
+(defn cont-frac-stats
+  [n]
+  (let [cf (cont-frac/cont-frac-sqrt n)]
+   {:n n
+    :continued-fraction cf
+    :period (- (count cf) 1)}))
 
+(def cont-fracs (map cont-frac-stats (range 1000)))
+
+(println "\n=== Continued fractions from 1 to 1,000 ===\n")
+
+(p/print-table (sort-by :period cont-fracs))
