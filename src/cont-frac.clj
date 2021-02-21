4fd9ceae7fad7f044874ffbd175f2aa1752250d6
James Simmonds <james.e.simmonds@gmail.com>
2021-02-21 16:32:14 +0000

    Support square numbers

--- src/cont-frac.clj
+++ src/cont-frac.clj
@@ -17,8 +17,9 @@
 (defn cont-frac-sqrt
   "Find a continued fraction representation for a square root"
   [n]
-  (let [surd (vector 0 n 1)]
-    (find-cont-frac surd (surds/int-part surd))))
+  (if (= (mod (math/sqrt n) 1) 0) (vector (math/sqrt n))
+   (let [surd (vector 0 n 1)]
+    (find-cont-frac surd (surds/int-part surd)))))
 
 (deftest surd-tests
   (testing "Calculating continued fractions for surds"
@@ -26,4 +27,7 @@
     (is (= (find-cont-frac [0 431 1] 20) [20 1 3 5 1 2 7 1 19 1 7 2 1 5 3 1 40])))
   (testing "Calculating continued fractions for square roots"
     (is (= (cont-frac-sqrt 15) [3 1 6]))
-    (is (= (cont-frac-sqrt 222) [14 1 8 1 28]))))
+    (is (= (cont-frac-sqrt 222) [14 1 8 1 28]))
+    (is (= (cont-frac-sqrt 1) [1]))
+    (is (= (cont-frac-sqrt 16) [4]))
+    (is (= (cont-frac-sqrt 300) [17 3 8 3 34]))))
